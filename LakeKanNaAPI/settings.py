116ec90d11bb04120051560aceecf1cd1fb82cbf
cookieung <ung_1002@hotmail.com>
2018-03-28 22:31:52 +0700

    Try to fix css in cloud again

--- LakeKanNaAPI/settings.py
+++ LakeKanNaAPI/settings.py
@@ -122,5 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+
+STATIC_ROOT = '/home/django/django_project/django_project/static'
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
