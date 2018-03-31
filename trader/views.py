2237179cb68e2407436ebbd48ef66d9d9442c7d5
cookieung <ung_1002@hotmail.com>
2018-03-31 19:37:49 +0700

    Make filtering in each path

--- trader/views.py
+++ trader/views.py
@@ -34,6 +34,18 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
 class TradingList(generics.ListCreateAPIView):
     queryset = Trading.objects.all()
     serializer_class = TradingSerializer
+    def get_queryset(self):
+        queryset = Trading.objects.all()
+        owner = self.request.query_params.get('owner',None)
+        receiver = self.request.query_params.get('receiver',None)
+        tag = self.request.query_params.get('tag',None)
+        if owner is not None:
+            queryset = queryset.filter(owner=owner)
+        if receiver is not None:
+            queryset = queryset.filter(receiver=receiver)
+        if tag is not None:
+            queryset = queryset.filter(tags=tag)
+        return queryset
 
 class TradingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Trading.objects.all()
@@ -52,6 +64,12 @@ class TagDetail(generics.RetrieveUpdateDestroyAPIView):
 class BasketList(generics.ListCreateAPIView):
     queryset = Basket.objects.all()
     serializer_class = BasketSerializer
+    def get_queryset(self):
+        queryset = Basket.objects.all()
+        user = self.request.query_params.get('user',None)
+        if user is not None:
+            queryset = queryset.filter(owner=user)
+        return queryset
 
 class BasketDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Basket.objects.all()
@@ -85,15 +103,29 @@ class InventoryDetail(generics.RetrieveUpdateDestroyAPIView):
 class ReviewList(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    def get_queryset(self):
+        queryset = Review.objects.all()
+        writer = self.request.query_params.get('writer',None)
+        trade = self.request.query_params.get('trade',None)
+        if writer is not None:
+            queryset = queryset.filter(writer=writer)
+        if trade is not None:
+            queryset = queryset.filter(trade_id=trade)
+        return queryset
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-
 class ItemList(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    def get_queryset(self):
+        queryset = Item.objects.all()
+        inventory = self.request.query_params.get('inventory',None)
+        if inventory is not None:
+            queryset = queryset.filter(inventory_id=inventory)
+        return queryset
 
 class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Item.objects.all()
